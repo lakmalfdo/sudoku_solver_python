3f1369e5691f954bcbee6fe816f555cf327723ef
Lakmal Fernando <lakmalfdo@gmail.com>
2023-10-17 13:32:08 +0530

    Developed a Python Sudoku Solver project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Sudoku Solver
+
+## Introduction
+Welcome to Sudoku Solver, a Python project I developed to showcase the power of Python in solving Sudoku puzzles. This project focuses on efficiently solving Sudoku puzzles using backtracking algorithms and organizing the code in a class-based structure.
+
+## Development
+Sudoku Solver was developed using Python and Visual Studio Code, making it an excellent project for both beginners and Python enthusiasts to learn and hone their Python skills.
+
+## Main Features
+- **Dynamic Puzzle Generation**: The solver generates a dynamically created puzzle every time, adding variety and complexity to the experience.
+- **Backtracking Algorithm**: Utilizing a backtracking algorithm, the solver efficiently finds solutions, highlighting my strong understanding of algorithms and problem-solving skills.
+- **Class-Based Organization**: I structured the code using classes and methods to emphasize the importance of well-organized and encapsulated code logic, promoting maintainability and readability.
+
+## How to Use
+1. To get started, clone this repository to your local machine using the following command:
+<pre>git clone https://github.com/lakmalfdo/sudoku_solver_python.git</pre>
+
+
+2. Ensure Python is installed on your computer.
+
+3. Open your terminal and navigate to the project directory:
+<pre>cd sudoku_solver_python</pre>
+
+4. Launch the Sudoku Solver by running:
+<pre>python sudoku_solver.py</pre>
+
+## How to Modify
+
+1. To customize this project, clone the repository to your local machine:
+<pre>git clone https://github.com/lakmalfdo/sudoku_solver_python.git</pre>
+
+2. Open the project in your preferred code editor, such as Visual Studio Code.
+
+3. Modify the solver to meet your specific needs. Add new features, enhance the solving algorithm, or adapt the code for unique requirements. This project serves as a testament to the flexibility and modularity of Python code.
+
+Enjoy using and customizing the Sudoku Solver and let it serve as an illustration of my Python programming skills and my dedication to creating well-structured, functional solutions. Happy coding!
---
+++ sudoku_solver.py
@@ -0,0 +1,81 @@
+import random
+
+class SudokuSolver:
+    # Generates a new Sudoku puzzle with a unique solution.
+    def generate_sudoku(self):
+        # Initialize an empty 9x9 grid
+        self.sudoku = [[0 for _ in range(9)] for _ in range(9)]
+        
+        # Start with a solved puzzle
+        self.solve()
+        
+        # Remove random numbers to create a puzzle
+        # Adjust the difficulty by controlling the number of removed elements
+        # Adjust the range for the desired difficulty
+        for _ in range(random.randint(35, 55)):  
+            row = random.randint(0, 8)
+            col = random.randint(0, 8)
+            while self.sudoku[row][col] == 0:
+                row = random.randint(0, 8)
+                col = random.randint(0, 8)
+            self.sudoku[row][col] = 0
+
+    # Prints the Sudoku grid in a readable format.
+    def print_sudoku(self):
+        for i in range(9):
+            if i % 3 == 0 and i != 0:
+                print("-" * 21)
+            for j in range(9):
+                if j % 3 == 0 and j != 0:
+                    print("|", end=" ")
+                print(self.sudoku[i][j], end=" ")
+            print()
+
+    # Checks if placing number at a given position is valid.
+    def is_valid_move(self, row, col, num):
+        for i in range(9):
+            if self.sudoku[row][i] == num or self.sudoku[i][col] == num:
+                return False
+
+        subgrid_row = 3 * (row // 3)
+        subgrid_col = 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if self.sudoku[subgrid_row + i][subgrid_col + j] == num:
+                    return False
+
+        return True
+
+    # Recursively solves the Sudoku puzzle using a backtracking algorithm.
+    def solve(self):
+        for row in range(9):
+            for col in range(9):
+                if self.sudoku[row][col] == 0:
+                    for num in range(1, 10):
+                        if self.is_valid_move(row, col, num):
+                            self.sudoku[row][col] = num
+                            if self.solve():
+                                return True
+                            self.sudoku[row][col] = 0
+                    return False
+        return True
+
+# Create a SudokuSolver instance
+solver = SudokuSolver()
+
+# Generates a new unsolved Sudoku puzzle
+solver.generate_sudoku()
+
+print("Unsolved Sudoku:")
+# Show Unsolved Sudoku puzzle
+solver.print_sudoku()
+
+# Prompt the user to press any key to continue
+input("\nPress any key to solve the Sudoku...")
+
+# Solved the puzzle
+solver.solve()
+
+print("\nSolved Sudoku:")
+# Show Solved Sudoku puzzle
+solver.print_sudoku()
